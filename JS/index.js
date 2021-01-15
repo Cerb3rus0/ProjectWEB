86cfb71ef7f48e117940397a9e85c7ae5c7463d7
cerb3rus0 <cerb3rus0@yandex.ru>
2021-01-15 18:40:42 +0300

    new_code_1501

--- JS/index.js
+++ JS/index.js
@@ -73,13 +73,61 @@ length_arr();
 
 
 function tea(name, message){
-  
+
   var name = prompt("Your name?", 'My name is');
   var message = prompt('Your message', 'I have write message');
   let yesoryes = confirm('Ты пидор?');
 
+    if(yesoryes == true){
+      alert("Реально пидор"+ " "+ 'Haha')
+    }
+    else{
+      alert("Не пидор")
+    }
+
   alert(name+ ' ' + message)
-  console.log(yesoryes+' реально пидор');
+
   console.log(name+ ' ' + message);
 };
+
+
+function user_write(message_user_write, name_user_write) {
+
+  var message_user_write = prompt('What u talk?','Im gay');
+  var name_user_write = prompt('Whats u name?', 'Jhonny');
+
+  alert(message_user_write +" "+ name_user_write);
+  console.log(message_user_write +" "+ name_user_write);
+}
+
+
+const c = '17';
+const d = "17";
+const e = "I'm a string";
+const f = false;
+const g = true;
+const h = null;
+const i = undefined;
+const j = Symbol("I'm a Symbol");
+const k = { name: 'John' };
+const l = [1, 2, 3, 4];
+const m = function run() {
+    return 17;
+};
+
+let const_all_variable = [c,d,e,f,g,h,i,j,k,l,m];
+
+let num_variable = 0;
+
+function alert_typeof_variable(variable_name){
+  console.log('variable typeof:', typeof variable_name);
+}
+
+while(num_variable <= 10){
+  num_variable++;
+  
+  console.log(alert_typeof_variable(const_all_variable[num_variable]));
+}
+
+alert_typeof_variable()
 
